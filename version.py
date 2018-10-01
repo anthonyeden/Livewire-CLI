425bf6f7755774754a3f1ca0875ea5d24eda75ac
Anthony Eden <anthony@mediarealm.com.au>
2018-10-01 21:21:34 +1000

    Add build instructions, version, and icon.

---
+++ version.py
@@ -0,0 +1,28 @@
+VSVersionInfo(
+  ffi=FixedFileInfo(
+    filevers=(1, 0, 0, 0),
+    prodvers=(1, 0, 0, 0),
+    mask=0x3f,
+    flags=0x0,
+    OS=0x40004,
+    fileType=0x1,
+    subtype=0x0,
+    date=(0, 0)
+    ),
+  kids=[
+    StringFileInfo(
+      [
+      StringTable(
+        u'040904B0',
+        [StringStruct(u'CompanyName', u'Anthony Eden / Media Realm'),
+        StringStruct(u'FileDescription', u'Livewire CLI'),
+        StringStruct(u'FileVersion', u'1.0.0)'),
+        StringStruct(u'InternalName', u'Livewire-CLI'),
+        StringStruct(u'LegalCopyright', u'\xa9 Anthony Eden. All rights reserved.'),
+        StringStruct(u'OriginalFilename', u'Livewire-CLI.exe'),
+        StringStruct(u'ProductName', u'Livewire Command Line Interface'),
+        StringStruct(u'ProductVersion', u'1.0.0.0')])
+      ]), 
+    VarFileInfo([VarStruct(u'Translation', [1033, 1200])])
+  ]
+)
